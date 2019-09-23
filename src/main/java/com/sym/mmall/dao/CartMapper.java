2afe6eff96e96dc9152829f2ba3ebbea772136c5
shiyumeng <shiyumengnk@163.com>
2019-09-23 19:31:35 +0800

    initialize the maven project and create dao/pojo/mappers using mybatis-generator-maven-plugin

---
+++ src/main/java/com/sym/mmall/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.sym.mmall.dao;
+
+import com.sym.mmall.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/sym/mmall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.sym.mmall.dao;
+
+import com.sym.mmall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
